6cd49b831281e6f5c164507eb79c6400a7a6b14a
Lutu-gl <lucaturin03@gmail.com>
2025-05-29 19:40:37 +0200

    solved day 16 👻

--- src/main/java/day16/Main.java
+++ src/main/java/day16/Main.java
@@ -11,15 +11,11 @@ import java.util.*;
  */
 public class Main {
 
-    private static final int[] DI = {0, 1, 0, -1}; // Directions: E, S, W, N
-    private static final int[] DJ = {1, 0, -1, 0};
-    private static final String[] DIRECTIONS = {"E", "S", "W", "N"};
-
     public static void main(String[] args) {
         try {
             char[][] input = readInput("src/main/java/day16/input.txt");
             System.out.println(solve1(deepCopy(input)));
-            //System.out.println(solve2(deepCopy(input)));
+            System.out.println(solve2(deepCopy(input)));
         } catch (IOException e) {
             System.err.println("Error reading input file: " + e.getMessage());
         }
@@ -62,62 +58,130 @@ public class Main {
                 }
             }
         }
-        //printArr(arr);
 
-        return dijkstra(arr, startI, startJ);
-    }
+        // run dijkstra with priority queue
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Set<List<Integer>> seen = new HashSet<>();
 
-    // direction 0 = east
-    public static long dijkstra(char[][] maze, int startI, int startJ) {
-        // Priority Queue
-        PriorityQueue<State> pq = new PriorityQueue<>(Comparator.comparing(s -> s.cost));
-        pq.add(new State(startI, startJ, 0, 0));
-
-        // visited with direction
-        boolean visited[][][] = new boolean[maze.length][maze[0].length][4];
+        pq.add(new Node(0, startI, startJ, 0, 1)); // facing east
 
         while (!pq.isEmpty()) {
-            State current = pq.poll();
-            int i = current.i;
-            int j = current.j;
-            int direction = current.direction;
-            int cost = current.cost;
-
-            if (maze[i][j] == 'E') {
-                return cost;
+            Node node = pq.poll();
+            seen.add(Arrays.asList(node.i, node.j, node.di, node.dj));
+            if (arr[node.i][node.j] == 'E') {
+               return node.cost;
             }
 
-            if (visited[i][j][direction]) {
-                continue;
-            }
-            visited[i][j][direction] = true;
+            List<Node> newNodes = new ArrayList<>();
+            newNodes.add(new Node(node.cost + 1,node.i + node.di, node.j + node.dj, node.di, node.dj)); // move in direction
+            newNodes.add(new Node(node.cost + 1000, node.i, node.j, node.dj, -node.di)); // rotate clockwise
+            newNodes.add(new Node(node.cost + 1000, node.i, node.j, - node.dj, node.di)); // rotate counterclockwise
 
-            //    private static final int[] DI = {0, 1, 0, -1};
-            //    private static final int[] DJ = {1, 0, -1, 0};
-            //    private static final String[] DIRECTIONS = {"E", "S", "W", "N"};
-            int newI = i + DI[direction];
-            int newJ = j + DJ[direction];
+            for (Node newNode : newNodes) {
+                if(newNode.i < 0 || newNode.i >= arr.length || newNode.j < 0 || newNode.j >= arr[0].length) continue;
+                if(arr[newNode.i][newNode.j] == '#') continue;
+                if(seen.contains(Arrays.asList(newNode.i, newNode.j, newNode.di, newNode.dj))) continue;
 
-            // 1. Move Forward
-            if(isValidMove(maze, newI, newJ)) {
-                pq.add(new State(newI, newJ, direction, cost + 1));
+                pq.add(newNode);
             }
 
-            // 2. Rotate Clockwise
-            int newDirection = (direction + 1) % 4;
-            pq.add(new State(i, j, newDirection, cost + 1000));
-
-            // 3. Rotate Counter-Clockwise
-            newDirection = (direction + 3) % 4;
-            pq.add(new State(i, j, newDirection, cost + 1000));
         }
-
-        return -1L;
+        return -1;
     }
 
+    public static long solve2(char[][] arr) {
+        long cost = -1;
+        int startI = -1;
+        int startJ = -1;
 
-    private static boolean isValidMove(char[][] maze, int i, int j) {
-        return i >= 0 && i < maze.length && j >= 0 && j < maze[0].length && maze[i][j] != '#';
+        // find starting position
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j] == 'S') {
+                    startI = i;
+                    startJ = j;
+                    break;
+                }
+            }
+        }
+
+        // run dijkstra with priority queue
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Map<Node, Integer> costs = new HashMap<>(); // node -> cost instead of seen set
+        Map<Node, Set<Node>> backtrack = new HashMap<>(); // backtrack not only one but could be a set of nodes.
+        Set<Node> endNodes = new HashSet<>();
+        pq.add(new Node(0, startI, startJ, 0, 1)); // facing east
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            if(costs.containsKey(node)) {
+                if (costs.get(node) < node.cost) continue;      // if greater do not consider.
+            } else {
+                costs.put(node, node.cost);
+            }
+            if (arr[node.i][node.j] == 'E') {
+                if (cost == -1 || node.cost < cost) {
+                    cost = node.cost;
+                    endNodes = new HashSet<>();
+                    endNodes.add(node);
+                } else if (node.cost == cost) {
+                    endNodes.add(node);
+                }
+            }
+
+
+            List<Node> newNodes = new ArrayList<>();
+            newNodes.add(new Node(node.cost + 1,node.i + node.di, node.j + node.dj, node.di, node.dj)); // move in direction
+            newNodes.add(new Node(node.cost + 1000, node.i, node.j, node.dj, -node.di)); // rotate clockwise
+            newNodes.add(new Node(node.cost + 1000, node.i, node.j, - node.dj, node.di)); // rotate counterclockwise
+
+            for (Node newNode : newNodes) {
+                if(newNode.i < 0 || newNode.i >= arr.length || newNode.j < 0 || newNode.j >= arr[0].length) continue;
+                if(arr[newNode.i][newNode.j] == '#') continue;
+                if(costs.containsKey(newNode)) {
+                    if (costs.get(newNode) < newNode.cost){
+                        continue;
+                    } else if (costs.get(newNode) > newNode.cost) {
+                        costs.put(newNode, newNode.cost);
+                        backtrack.put(newNode, new HashSet<>());
+                    }
+
+                    Set<Node> newSet = backtrack.get(newNode);
+                    newSet.add(node);
+                    backtrack.put(newNode, newSet);
+                } else {
+                    Set<Node> newSet = new HashSet<>();
+                    if (backtrack.containsKey(newNode)) {
+                        newSet = backtrack.get(newNode);
+                    }
+                    newSet.add(node);
+                    backtrack.put(newNode, newSet);
+                    costs.put(newNode, newNode.cost);
+                }
+                pq.add(newNode);
+            }
+        }
+
+        Queue<Node> queue = new LinkedList<>(endNodes);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (arr[node.i][node.j] == 'S') continue;  // found last element
+            arr[node.i][node.j] = 'O';
+            for (Node newNode : backtrack.get(node)) {
+                queue.add(newNode);
+            }
+        }
+        //printArr(arr);
+
+        int tilesSolution=1; // because of start
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if(arr[i][j] == 'O') tilesSolution++;
+            }
+        }
+
+        return tilesSolution;
     }
 
     private static void printArr(char[][] arr) {
@@ -142,24 +206,6 @@ public class Main {
         }
     }
 
-    private static void printArr(int[][] arr) {
-        final int CELL_WIDTH = 6; // Maximale Breite pro Zelle
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                String value;
-                if (arr[i][j] == Integer.MAX_VALUE) {
-                    value = "X"; // Platzhalter für unerreichbare Felder
-                } else {
-                    value = String.valueOf(arr[i][j]);
-                }
-                // Die Ausgabe wird mit fester Breite ausgerichtet
-                System.out.printf("%-" + CELL_WIDTH + "s", value);
-            }
-            System.out.println();
-        }
-    }
-
 
     public static char[][] deepCopy(char[][] original) {
         if (original == null) {
@@ -174,13 +220,47 @@ public class Main {
     }
 }
 
-class State {
-    int i, j, direction, cost;
+class Node implements Comparable<Node> {
+    int cost;
+    int i;
+    int j;
+    int di; // direction i
+    int dj; // direction j
 
-    public State(int i, int j, int direction, int cost) {
+    public Node(int cost,int i,int j,int di,int dj) {
+        this.cost = cost;
         this.i = i;
         this.j = j;
-        this.direction = direction;
-        this.cost = cost;
+        this.di = di;
+        this.dj = dj;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return Integer.compare(this.cost, o.cost);
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "cost=" + cost +
+                ", i=" + i +
+                ", j=" + j +
+                ", di=" + di +
+                ", dj=" + dj +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Node node = (Node) obj;
+        return i == node.i && j == node.j && di == node.di && dj == node.dj;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(i, j, di, dj); // to make hashmap work as intended don't consider cost
     }
 }
