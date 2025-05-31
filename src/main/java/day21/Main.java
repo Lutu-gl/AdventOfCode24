292c431a3063158b7c6623a2a26e150a26354145
Lutu-gl <lucaturin03@gmail.com>
2025-05-31 11:10:23 +0200

    solved day 21 part 1 🎅

--- src/main/java/day21/Main.java
+++ src/main/java/day21/Main.java
@@ -6,6 +6,17 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main {
+    static char[][] keypadBig = {
+            {'7', '8', '9'},
+            {'4', '5', '6'},
+            {'1', '2', '3'},
+            {'-', '0', 'A'}
+    };
+    static char[][] keypadSmall = {
+            {'-', '^', 'A'},
+            {'<', 'v', '>'},
+    };
+
     public static void main(String[] args) {
         try {
             List<String> input = readInput("src/main/java/day21/input.txt");
@@ -28,89 +39,122 @@ public class Main {
     }
 
     public static long solve1(List<String> input) {
-        List<String> codes = parseCodes(input);
-        System.out.println(input);
         long sum = 0L;
         for (int i = 0; i < input.size(); i++) {
             String code = input.get(i);
-            List<Character> shortestSequenceTotalForCode = getShortestSequenceTotalForCode(code);
-            sum += (long) shortestSequenceTotalForCode.size() * extractNumericValue(code);
-            System.out.println("Code: " + code + " - " + shortestSequenceTotalForCode.size() + " - " + extractNumericValue(code) + " - " + shortestSequenceTotalForCode.size() * extractNumericValue(code));
+            String shortestSequenceTotalForCode = getShortestSequenceTotalForCode1(code);
+            sum += (long) shortestSequenceTotalForCode.length() * extractNumericValue(code);
         }
 
         return sum;
     }
 
-    public static List<Character> getShortestSequenceTotalForCode (String code) {
-        List<Character> shortestSequenceTotal;
-        char[][] keypadBig = {
-                {'7', '8', '9'},
-                {'4', '5', '6'},
-                {'1', '2', '3'},
-                {'-', '0', 'A'}
-        };
-        char[][] keypadSmall = {
-                {'-', '^', 'A'},
-                {'<', 'v', '>'},
-        };
-        // 456A
-        // ^^<<A>A>AvvA
-        // <AAv<AA>>^AvA^AvA^Av<AA>^A
-        //
+    public static String getShortestSequenceTotalForCode1(String code) {
+        List<String> shortestSequencesTotal;
+
+
         Robot robotBigKeypad = new Robot(3, 2, keypadBig);
         Robot robotSmallKeypad1 = new Robot(0, 2, keypadSmall);
         Robot robotSmallKeypad2 = new Robot(0, 2, keypadSmall);
 
-        List<Character> codeList = new ArrayList<>();
-        for (int i = 0; i < code.length(); i++) {
-            codeList.add(code.charAt(i));
+        // big Keypad
+        shortestSequencesTotal = getShortestSequencesForCodeAndRobot(code, robotBigKeypad);
+
+        // small Keypad1
+        List<String> newTotalList = new ArrayList<>();
+        for (String s : shortestSequencesTotal) {
+            shortestSequencesTotal = getShortestSequencesForCodeAndRobot(s, robotSmallKeypad1);
+            newTotalList.addAll(shortestSequencesTotal);
         }
+        shortestSequencesTotal = newTotalList;
 
-        //<<vA>>^A<A>AvA<^AA>A<vAAA>^A
-        //<<vAA>A>^AvAA<^A>A<<vA>>^AvA^A<vA>^A<<vA>^A>AAvA^A<<vA>A>^AAAvA<^A>A
+        // remove length greater
+        filterMinLength(shortestSequencesTotal);
 
-        //<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-        shortestSequenceTotal = getShortestSequenceForCodeAndRobot(codeList, robotBigKeypad);
-        System.out.println(convertListCharToString(shortestSequenceTotal));
-        shortestSequenceTotal = getShortestSequenceForCodeAndRobot(shortestSequenceTotal, robotSmallKeypad1);
-        System.out.println(convertListCharToString(shortestSequenceTotal));
-        shortestSequenceTotal = getShortestSequenceForCodeAndRobot(shortestSequenceTotal, robotSmallKeypad2);
-        System.out.println(convertListCharToString(shortestSequenceTotal));
+        // smallKeypad2
+        newTotalList = new ArrayList<>();
+        for (String s : shortestSequencesTotal) {
+            shortestSequencesTotal = getShortestSequencesForCodeAndRobot(s, robotSmallKeypad2);
+            newTotalList.addAll(shortestSequencesTotal);
+        }
+        shortestSequencesTotal = newTotalList;
 
-        return shortestSequenceTotal;
+        filterMinLength(shortestSequencesTotal);
+
+        String s = shortestSequencesTotal.stream().min((e1, e2) -> e1.length() - e2.length()).get();
+        return s;
     }
-    public static List<Character> getShortestSequenceForCodeAndRobot(List<Character> code, Robot robot) {
-        List<Character> shortestSequenceTotal = new ArrayList<>();
-        for (int j = 0; j < code.size(); j++) {
-            char c = code.get(j);
-            List<Character> ss = robot.getSequenceTo(c);
-            shortestSequenceTotal.addAll(ss);
-            shortestSequenceTotal.add('A');
+
+    private static void filterMinLength(List<String> shortestSequencesTotal) {
+        int minLen = shortestSequencesTotal.stream()
+                .mapToInt(String::length)
+                .min()
+                .orElse(0);
+        shortestSequencesTotal.removeIf(str -> str.length() > minLen);
+    }
+
+    public static List<String> getShortestSequencesForCodeAndRobot(String code, Robot robot) {
+        List<String> shortestSequencesTotal = new ArrayList<>();
+        for (int j = 0; j < code.length(); j++) {
+            char c = code.charAt(j);
+            List<String> sequencesForChar = robot.getSequenceTo(c);
+            if(shortestSequencesTotal.isEmpty()) {
+                for (String s : sequencesForChar) {
+                    shortestSequencesTotal.add(s + 'A');
+                }
+            } else {
+                List<String> newShortestSequencesTotal = new ArrayList<>();
+                for (String already : shortestSequencesTotal) {
+                    for (String charSequence : sequencesForChar) {
+                        newShortestSequencesTotal.add(already + charSequence + 'A');
+                    }
+                }
+                shortestSequencesTotal = newShortestSequencesTotal;
+            }
         }
-        return shortestSequenceTotal;
+        return shortestSequencesTotal;
     }
 
     public static long solve2(List<String> input) {
-        // Placeholder for Part 2 logic, if needed
-        return 0;
+        long sum = 0L;
+        for (int i = 0; i < input.size(); i++) {
+            String code = input.get(i);
+            String shortestSequenceTotalForCode = getShortestSequenceTotalForCode2(code);
+            sum += (long) shortestSequenceTotalForCode.length() * extractNumericValue(code);
+            //System.out.println("Code: " + code + " - " + shortestSequenceTotalForCode.length() + " - " + extractNumericValue(code) + " - " + shortestSequenceTotalForCode.length() * extractNumericValue(code));
+        }
+
+        return sum;
     }
 
-    private static List<String> parseCodes(List<String> input) {
+    public static String getShortestSequenceTotalForCode2(String code) {
+        List<String> shortestSequencesTotal = new ArrayList<>();
 
+        Robot robotBigKeypad = new Robot(3, 2, keypadBig);
+        List<Robot> robots = new ArrayList<>();
+        robots.add(robotBigKeypad);
+        for (int i = 0; i < 2; i++) { // 3 is already too big.. have to find another way to not work with strings.
+            robots.add( new Robot(0, 2, keypadSmall));
+        }
 
-        return input;
+        shortestSequencesTotal.add(code);
+        for (Robot robot : robots) {
+            List<String> newTotalList = new ArrayList<>();
+            for (String s : shortestSequencesTotal) {
+                shortestSequencesTotal = getShortestSequencesForCodeAndRobot(s, robot);
+                newTotalList.addAll(shortestSequencesTotal);
+            }
+            shortestSequencesTotal = newTotalList;
+            filterMinLength(shortestSequencesTotal);
+        }
+
+        String s = shortestSequencesTotal.stream().min((e1, e2) -> e1.length() - e2.length()).get();
+        return s;
     }
+
 
     private static int extractNumericValue(String code) {
         String numericPart = code.replaceAll("[^0-9]", "");
         return Integer.parseInt(numericPart);
-    }
-
-    public static String convertListCharToString(List<Character> list) {
-        StringBuilder sb = new StringBuilder();
-        for (Character c : list) {
-            sb.append(c);
-        }
-        return sb.toString();
     }
 }
--- src/main/java/day21/Robot.java
+++ src/main/java/day21/Robot.java
@@ -1,7 +1,6 @@
 package day21;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Robot {
     int positionI;
@@ -27,113 +26,56 @@ public class Robot {
 
     // moving is done with >, <, v, and ^
     // I cant move through '-'
-    public List<Character> getSequenceTo(Character c) {
-        int targetI = -1;
-        int targetJ = -1;
+    public List<String> getSequenceTo(Character c) {
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(positionI, positionJ, ""));
 
-        for (int i = 0; i < keypad.length; i++) {
-            for (int j = 0; j < keypad[0].length; j++) {
-                if (keypad[i][j] == c) {
-                    targetI = i;
-                    targetJ = j;
-                    break;
-                }
+        List<Node> endNodes = new ArrayList<>();
+        int optimalCost = Integer.MAX_VALUE;
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+
+            if (keypad[node.i][node.j] == c) {
+                // found sequence
+                endNodes.add(node);
+                optimalCost = node.getCost();
+                continue;
+            }
+            if (node.getCost() >= optimalCost) continue; // can be greater or equal because newNodes are all cost + 1
+
+            List<Node> newNodes = new ArrayList<>();
+            newNodes.add(new Node(node.i + 1, node.j, node.sequence + "v"));
+            newNodes.add(new Node(node.i - 1, node.j, node.sequence + "^"));
+            newNodes.add(new Node(node.i, node.j + 1, node.sequence + ">"));
+            newNodes.add(new Node(node.i, node.j - 1, node.sequence + "<"));
+
+            for (Node newNode : newNodes) {
+                if (newNode.i >= keypad.length || newNode.i < 0 || newNode.j < 0 || newNode.j>= keypad[0].length) continue;
+                if (keypad[newNode.i][newNode.j] == '-') continue;
+                queue.add(newNode);
             }
         }
+        positionI = endNodes.get(0).i;
+        positionJ = endNodes.get(0).j;
 
+        List<String> sequences = endNodes.stream().map(e -> e.sequence).toList();
 
+        return sequences;
+    }
+}
+// probably add a new class with int int string (and then at the end I've found a List of endNodes<> with all the same cost and easy.
+class Node {
+    int i;
+    int j;
+    String sequence = "";
 
-        int moveJ = targetJ - positionJ;
-        int moveI = targetI - positionI;
+    public Node(int i, int j, String sequence) {
+        this.i = i;
+        this.j = j;
+        this.sequence = sequence;
+    }
 
-
-        List<Character> moveSequence = new ArrayList<>();
-        if(notAllowedI == 3 && notAllowedJ == 0) {
-            if (moveI < 0) {
-                while (moveI != 0) {
-                    if (moveI > 0) {
-                        moveSequence.add('v');
-                        moveI--;
-                    } else {
-                        moveSequence.add('^');
-                        moveI++;
-                    }
-                }
-                while (moveJ != 0) {
-                    if (moveJ > 0) {
-                        moveSequence.add('>');
-                        moveJ--;
-                    } else {
-                        moveSequence.add('<');
-                        moveJ++;
-                    }
-                }
-            } else {
-                while (moveJ != 0) {
-                    if (moveJ > 0) {
-                        moveSequence.add('>');
-                        moveJ--;
-                    } else {
-                        moveSequence.add('<');
-                        moveJ++;
-                    }
-                }
-                while (moveI != 0) {
-                    if (moveI > 0) {
-                        moveSequence.add('v');
-                        moveI--;
-                    } else {
-                        moveSequence.add('^');
-                        moveI++;
-                    }
-                }
-            }
-        }
-
-        if (notAllowedI == 0 && notAllowedJ == 0) {
-            if (moveI < 0) {
-                while (moveJ != 0) {
-                    if (moveJ > 0) {
-                        moveSequence.add('>');
-                        moveJ--;
-                    } else {
-                        moveSequence.add('<');
-                        moveJ++;
-                    }
-                }
-                while (moveI != 0) {
-                    if (moveI > 0) {
-                        moveSequence.add('v');
-                        moveI--;
-                    } else {
-                        moveSequence.add('^');
-                        moveI++;
-                    }
-                }
-            } else {
-                while (moveI != 0) {
-                    if (moveI > 0) {
-                        moveSequence.add('v');
-                        moveI--;
-                    } else {
-                        moveSequence.add('^');
-                        moveI++;
-                    }
-                }
-                while (moveJ != 0) {
-                    if (moveJ > 0) {
-                        moveSequence.add('>');
-                        moveJ--;
-                    } else {
-                        moveSequence.add('<');
-                        moveJ++;
-                    }
-                }
-            }
-        }
-
-        positionI = targetI;
-        positionJ = targetJ;
-        return moveSequence;
+    public int getCost(){
+        return sequence.length();
     }
 }
